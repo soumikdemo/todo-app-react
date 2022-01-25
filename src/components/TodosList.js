97a5c790419464c9521799e1afd132ece6ab9fa5
Soumik Datta <soumik@Aarongcs.com>
2022-01-25 17:10:30 +0600

    initial commit

---
+++ src/components/TodosList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import TodoItem from './TodoItem';
+
+//class-based component
+class TodosList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map(todo => (
+          //<li key={todo.id}>{todo.title}</li>
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    )
+  }
+}
+
+export default TodosList
