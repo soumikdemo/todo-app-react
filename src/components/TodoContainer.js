dd05ffd58d582d147ec2d1bb87cf1c824d861431
Soumik Datta <soumik@Aarongcs.com>
2022-01-26 17:27:55 +0600

    add checkboxes and delete buttons

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -23,11 +23,38 @@ class TodoContainer extends React.Component {
     ]
    };
 
+  handleChange = (id) =>{
+    this.setState(prevState =>({
+      todos: prevState.todos.map(todo => {
+        if(todo.id === id){
+          //to satisfy react-strict-mode
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+
+        //array.map() returns an array
+        return todo;
+      })
+    }));
+  }
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} deleteTodoProps={this.delTodo} />
       </div>
     )
   }
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -7,8 +7,7 @@ class TodosList extends React.Component {
     return (
       <ul>
         {this.props.todos.map(todo => (
-          //<li key={todo.id}>{todo.title}</li>
-          <TodoItem key={todo.id} todo={todo} />
+          <TodoItem key={todo.id} todo={todo} handleChangeProps={this.props.handleChangeProps} deleteTodoProps={this.props.deleteTodoProps} />
         ))}
       </ul>
     )
